7fc63b4428e4da91d57d55b58276ee569e8b5762
Alejandro Caballero <lava.caballero@gmail.com>
2016-07-29 00:35:19 -0500

    Debugging

--- index.php
+++ index.php
@@ -5,10 +5,16 @@
  * @package    BardCanvas
  * @subpackage posts
  * @author     Alejandro Caballero - lava.caballero@gmail.com
+ * @var template $template
+ * @var account  $account
  */
+
+use hng2_base\account;
+use hng2_base\template;
 
 include "../config.php";
 include "../includes/bootstrap.inc";
+if( ! $account->_exists ) throw_fake_404();
 
 $template->page_contents_include = "contents/index.inc";
 $template->set_page_title($current_module->language->index->title);
